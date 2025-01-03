d487ec3a6860ffaf01c7b18958d00d6b9d91aa7d
Le Sang <lesangbt99@gmail.com>
2025-01-03 15:58:16 +0700

    chore: resolve conflict

--- src/const/Url.js
+++ src/const/Url.js
@@ -1,5 +1,7 @@
 // backend url
 
+import { REFRESH_TOKEN } from "./LocalStorage";
+
 const Url = {
     // LOGIN: "/login",
     // REGISTER: "/register",
@@ -10,6 +12,9 @@ const Url = {
     CREATE_GAME: "/api/v1/games",
     GET_GAME_DETAIL: "/api/v1/games/detail/:id",
     GET_DEFAULT_GAME: "/api/v1/games/default",
+    GET_NEW_TOKEN: "api/v1/refresh-token",
+    DELETE_EVENT: (id) => `api/v1/events/delete/${id}`,
+    UPDATE_EVENT: (id) => `api/v1/events/update/${id}`,
     
 }
 
