48548e3f8bca250c0f11de7c9603c7fa8fe7853e
Le Sang <lesangbt99@gmail.com>
2025-01-13 19:34:40 +0700

    feat: update game

--- src/const/Url.js
+++ src/const/Url.js
@@ -10,11 +10,13 @@ const Url = {
     GET_EVENT_DETAIL: "/api/v1/events/:id",
     GET_GAME: "/api/v1/games",
     CREATE_GAME: "/api/v1/games",
+    UPDATE_GAME: (id) => `/api/v1/games/${id}`,
     GET_GAME_DETAIL: "/api/v1/games/detail/:id",
     GET_DEFAULT_GAME: "/api/v1/games/default",
     GET_NEW_TOKEN: "api/v1/refresh-token",
     DELETE_EVENT: (id) => `api/v1/events/delete/${id}`,
     UPDATE_EVENT: (id) => `api/v1/events/update/${id}`,
+    UPLOAD: "/api/v1/upload",
     
 }
 
